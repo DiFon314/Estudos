d6ef9034b6446e4181cd8cd1a6daa401f666ad9a
Diogo Fernandes Coucello da Fonseca <Diogo.coucello@gmail.com>
2022-06-28 15:06:00 -0400

    a

--- Front_End/Engenharia_na_pratica/script/fasores.js
+++ Front_End/Engenharia_na_pratica/script/fasores.js
@@ -6,14 +6,30 @@ var x = document.getElementById('x')
 var y = document.getElementById('y')
 
 function convert(){
-    if(mod.value!=''&& ang.value!=''){
+    if((mod.value==''&& ang.value==''&& x.value==''&&y.value=='')// todos sem preenchimento
+    ||
+    (mod.value!=''&& ang.value==''&& x.value==''&&y.value=='')
+    ||
+    (mod.value==''&& ang.value!=''&& x.value==''&&y.value=='')
+    ||
+    (mod.value==''&& ang.value==''&& x.value!=''&&y.value=='')
+    ||
+    (mod.value==''&& ang.value==''&& x.value==''&&y.value!='')
+    ||
+    (mod.value!=''&& ang.value==''&& x.value!=''&&y.value=='')){
+        window.alert('Preencha corretamente os campos!!!')
+    }else if(mod.value!=''&& ang.value!=''){
     //Os dois são preenchidos polar para retangular
         let graus = ang.value * Math.PI/180;
         let cos = Math.cos(graus)
         let seno = Math.sin(graus)
         x.value= (mod.value * cos).toFixed(4)
         y.value = (mod.value * seno).toFixed(4)
+    }else if(x!=''&&y!=''){
+        mod.value = (Math.sqrt(x.value**2+y.value**2)).toFixed(4)
+        ang.value=(Math.atan(y.value/x.value)*180/Math.PI).toFixed(4)
     }
+    
 }
 
 but.addEventListener("click",convert);
---
+++ Front_End/Engenharia_na_pratica/script/resistores.js
@@ -0,0 +1,10 @@
+var quant=document.getElementById('quant')
+var but=document.getElementById('but')
+
+function adicionar (){
+    for(i=0;i<quant.value;i=i+1){
+        document.
+    }
+}
+
+but.addEventListener('click',adicionar)
