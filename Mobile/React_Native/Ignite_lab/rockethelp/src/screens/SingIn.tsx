e1a9060cc408cc037e95e59fee972a4a37a3b54b
Diogo Fernandes Coucello da Fonseca <Diogo.coucello@gmail.com>
2022-07-19 08:11:48 -0400

    funcionando aula2

--- Mobile/React_Native/Ignite_lab/rockethelp/src/screens/SingIn.tsx
+++ Mobile/React_Native/Ignite_lab/rockethelp/src/screens/SingIn.tsx
@@ -1,10 +1,16 @@
+import { useState } from "react";
 import { VStack, Heading, Icon, useTheme} from "native-base";
 
-import Logo from '../assets/logo_primary.svg';
+import Logo from '../assets/Primeiro.svg';
 
-import { Input } from "../components/input";
+import { Input } from "../components/Input";
+import { Button } from "../components/Button";
 import { Envelope, Key } from "phosphor-react-native";
+
 export function SingIn(){
+    const[name, setName]=useState('');
+    const[password, setPassword]=useState('');
+
     const{colors} =useTheme();
     return (
         <VStack flex={1} alignItems="center" bg="gray.600" px={8} pt={24}>
@@ -12,17 +18,21 @@ export function SingIn(){
             <Logo/>
 
             <Heading color="gray.100" fontSize="xl" mt={20} mb={6}>
-                Acesse sua conta
+                Acesse sua conta 
             </Heading>
 
             <Input 
                 placeholder="E-mail" 
                 marginBottom={4} 
-                InputLeftElement={<Icon as={<Envelope color={colors.gray[300]} />} ml={4}/>}/>
-            <Input placeholder="Senha"InputLeftElement={<Icon as={<Key color={colors.gray[300]} />} ml={4}/>}
+                InputLeftElement={<Icon as={<Envelope color={colors.gray[300]} />} ml={4}/>}
+                onChangeText={text=> setName(text)}
+                />
+            <Input placeholder="Senha"
+                marginBottom={8} InputLeftElement={<Icon as={<Key color={colors.gray[300]} />} ml={4}/>}
                 secureTextEntry
+                onChangeText={setPassword}
             />
-
+            <Button title="Entrar" w="full"/>
         </VStack>
     )
 }
