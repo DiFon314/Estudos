31edb08638f3c3b659864e106b4a2450a9bd61c0
Diogo Fernandes Coucello da Fonseca <Diogo.coucello@gmail.com>
2022-07-18 20:41:15 -0400

    ainda não funcionou

--- Mobile/React_Native/Ignite_lab/rockethelp/src/screens/SingIn.tsx
+++ Mobile/React_Native/Ignite_lab/rockethelp/src/screens/SingIn.tsx
@@ -9,7 +9,7 @@ export function SingIn(){
     return (
         <VStack flex={1} alignItems="center" bg="gray.600" px={8} pt={24}>
             
-            {/* <Logo/> */}
+            <Logo/>
 
             <Heading color="gray.100" fontSize="xl" mt={20} mb={6}>
                 Acesse sua conta
