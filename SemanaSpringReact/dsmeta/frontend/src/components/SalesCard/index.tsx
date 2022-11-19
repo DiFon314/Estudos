47c1df4874bd7b552fa17fda055f28bfb38286f0
Diogo Fernandes Coucello da Fonseca <Diogo.coucello@gmail.com>
2022-11-19 15:30:24 -0400

    useState

--- SemanaSpringReact/dsmeta/frontend/src/components/SalesCard/index.tsx
+++ SemanaSpringReact/dsmeta/frontend/src/components/SalesCard/index.tsx
@@ -1,24 +1,32 @@
-import NotificationButton from '../NotificationButton'
-import './styles.css'
+import { useState } from 'react';
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
+import NotificationButton from '../NotificationButton';
+import './styles.css';
+
 function SalesCard() {
+
+    const date = new Date(new Date().setDate(new Date().getDate() - 365));
+
+    const [minDate, setMinDate] = useState(date);
+    const [maxDate, setMaxDate] = useState(new Date());
+
     return (
         <div className="dsmeta-card">
             <h2 className="dsmeta-sales-title">Vendas</h2>
             <div>
               <div className="dsmeta-form-control-container">
               <DatePicker
-                    selected={new Date()}
-                    onChange={(date: Date) => {}}
+                    selected={minDate}
+                    onChange={(date: Date) => setMinDate(date)}
                     className="dsmeta-form-control"
                     dateFormat="dd/MM/yyyy"
                 />
               </div>
               <div className="dsmeta-form-control-container">
               <DatePicker
-                    selected={new Date()}
-                    onChange={(date: Date) => {}}
+                    selected={maxDate}
+                    onChange={(date: Date) => setMaxDate(date)}
                     className="dsmeta-form-control"
                     dateFormat="dd/MM/yyyy"
                 />
